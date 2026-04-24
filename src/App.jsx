cda75143e1a9d053eb04275d1b580cc2b283b02b
Wanessa Santos <wanessa.ufpe2018@gmail.com>
2026-04-24 19:55:28 -0300

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Lista de Tarefas</h1>
+      <p>Bem-vindo à sua lista de tarefas!</p>
+      <p><i>By <a href="https://github.com/wanessasantos360" target="_blank" rel="noopener noreferrer">Wanessa Santos</a></i></p>
+
+    </div>
+  )
+}
+export default App;
